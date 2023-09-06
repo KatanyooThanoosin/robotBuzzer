922e6d35192a133410296804a8aaea665ae56e21
BaitoeyKP <kanyarat_toey@hotmail.com>
2023-09-06 23:30:03 +0700

    Initial commit

---
+++ src/Temp.cpp
@@ -0,0 +1,15 @@
+#include "Temp.h"
+Temp::Temp(/* args */)
+{
+}
+
+Temp::~Temp()
+{
+}
+
+/// @brief tempFunc is template lib
+/// @param a is param frist
+/// @param b is param second
+void Temp::tempFunc(int a, int b)
+{
+}
---
+++ src/main.cpp
@@ -0,0 +1,13 @@
+#include <Arduino.h>
+#include "Temp.h"
+// put function declarations here:
+Temp temp=Temp();
+void setup() {
+  // put your setup code here, to run once:
+  temp.tempFunc(5,7);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
+
